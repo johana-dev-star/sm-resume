863c714a7dc8267df4fb372de98c128ccfb6569f
unknown <velasquez14191@email.com>
2026-04-06 17:25:10 +0300

    real console

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -60,8 +60,11 @@ export default async function handler(req, res) {
       const latest = experience.reduce((max, job) => {
         if(job.end_date == "Present") return new Date();
         const date = parseDate(job.end_date);
+        console.log(date)
         return date > max ? date : max;
       }, new Date(1900, 0, 1));
+
+      console.log(latest)
 
       const years = (latest - earliest) / (1000 * 60 * 60 * 24 * 365);
       return Math.round(years);
